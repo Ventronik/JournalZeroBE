6b35ea64485d1600c0f1f1ec551f414c03a21b2d
Dustin Ruskell <druskell@gmail.com>
2024-12-31 14:18:19 -0800

    removed a file which I think is no longer needed

--- db/seeds/00_clean.js
+++
@@ -1,7 +0,0 @@
-exports.seed = function(knex, Promise) {
-
-  const tablesToClean = ['paper_status', 'comments', 'papers', 'users']
-
-  return tablesToClean.reduce((acc, ele) => acc.then(() => knex(ele).del()), Promise.resolve())
-
-};
